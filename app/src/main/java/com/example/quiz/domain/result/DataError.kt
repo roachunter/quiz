01e2e724b75794490549f0659d14529ecf0ac3ae
Danone <mmmdanone04@gmail.com>
2025-04-17 14:27:36 +0300

    add necessary network errors

--- app/src/main/java/com/example/quiz/domain/result/DataError.kt
+++ app/src/main/java/com/example/quiz/domain/result/DataError.kt
@@ -2,6 +2,13 @@ package com.example.quiz.domain.result
 
 sealed interface DataError : Error {
     enum class Network : DataError {
+        NoInternet,
+        RequestTimeout,
 
+        InvalidDataFormat,
+        NoResults,
+        RateLimit,
+
+        Unknown
     }
 }
