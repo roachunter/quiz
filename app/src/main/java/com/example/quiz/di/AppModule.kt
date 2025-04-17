c8fb2e3956308038d63bdb54ff78ac539e6bab11
Danone <mmmdanone04@gmail.com>
2025-04-17 14:29:20 +0300

    register HttpClient and QuizProvider in appModule

--- app/src/main/java/com/example/quiz/di/AppModule.kt
+++ app/src/main/java/com/example/quiz/di/AppModule.kt
@@ -1,7 +1,20 @@
 package com.example.quiz.di
 
+import com.example.quiz.data.remote.OpenTDBQuizProvider
+import com.example.quiz.data.remote.QuizProvider
+import io.ktor.client.HttpClient
+import io.ktor.client.engine.cio.CIO
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
 import org.koin.dsl.module
 
 val appModule = module {
+    single<HttpClient> {
+        HttpClient(CIO) {
+            install(ContentNegotiation)
+        }
+    }
 
+    single<QuizProvider> {
+        OpenTDBQuizProvider(client = get())
+    }
 }
