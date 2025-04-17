26c1adac7db728951d631bcc987a94cc675c1637
Danone <mmmdanone04@gmail.com>
2025-04-17 16:13:49 +0300

    fix HttpClient config

--- app/src/main/java/com/example/quiz/di/AppModule.kt
+++ app/src/main/java/com/example/quiz/di/AppModule.kt
@@ -2,19 +2,26 @@ package com.example.quiz.di
 
 import com.example.quiz.data.remote.OpenTDBQuizProvider
 import com.example.quiz.data.remote.QuizProvider
+import com.example.quiz.presentation.QuizViewModel
 import io.ktor.client.HttpClient
 import io.ktor.client.engine.cio.CIO
 import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.serialization.kotlinx.json.json
+import org.koin.core.module.dsl.viewModelOf
 import org.koin.dsl.module
 
 val appModule = module {
     single<HttpClient> {
         HttpClient(CIO) {
-            install(ContentNegotiation)
+            install(ContentNegotiation) {
+                json()
+            }
         }
     }
 
     single<QuizProvider> {
         OpenTDBQuizProvider(client = get())
     }
+
+    viewModelOf(::QuizViewModel)
 }
