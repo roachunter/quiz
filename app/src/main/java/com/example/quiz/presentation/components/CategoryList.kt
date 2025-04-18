951fd2df032baf3ee3617f715c5056e3705dc830
Danone <mmmdanone04@gmail.com>
2025-04-18 13:31:18 +0300

    update layout with lazy column with sticky headers based on category group

--- app/src/main/java/com/example/quiz/presentation/components/CategoryList.kt
+++ app/src/main/java/com/example/quiz/presentation/components/CategoryList.kt
@@ -1,40 +1,80 @@
 package com.example.quiz.presentation.components
 
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.lazy.grid.GridCells
-import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
-import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import com.example.quiz.domain.category.Category
 import com.example.quiz.ui.theme.ColorSets
 
+@OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun CategoryList(
     categories: List<Category>,
     modifier: Modifier = Modifier
 ) {
-    LazyVerticalGrid(
-        columns = GridCells.Fixed(2),
-        horizontalArrangement = Arrangement.spacedBy(8.dp),
+    val groupedCategories = remember(categories) {
+        categories.groupBy {
+            it.group
+        }
+    }
+    LazyColumn(
+        horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.spacedBy(8.dp),
         modifier = modifier
     ) {
-        items(
-            items = categories,
-            key = { it.id }
-        ) { category ->
-            CategoryButton(
-                category = category,
-                colorSet = ColorSets.random,
-                onClick = {},
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .height(100.dp)
-            )
+        groupedCategories.forEach { (group, categories) ->
+            stickyHeader(key = group) {
+                Text(
+                    text = group,
+                    style = MaterialTheme.typography.headlineSmall,
+                    fontWeight = FontWeight.SemiBold,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .background(color = MaterialTheme.colorScheme.background)
+                        .padding(horizontal = 16.dp, vertical = 8.dp)
+                )
+            }
+
+            items(
+                items = categories.chunked(2),
+                key = { it.toString() }
+            ) { categoriesPair ->
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.spacedBy(8.dp)
+                ) {
+                    categoriesPair.forEach { category ->
+                        CategoryButton(
+                            category = category,
+                            colorSet = ColorSets.random,
+                            onClick = {},
+                            modifier = Modifier
+                                .weight(1f)
+                                .height(100.dp)
+                        )
+                    }
+                }
+            }
+        }
+
+
+        item {
+            Spacer(Modifier.height(200.dp))
         }
     }
 }
