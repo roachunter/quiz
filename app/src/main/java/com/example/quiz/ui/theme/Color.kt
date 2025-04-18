e70c81b385c2a5023399e72439b87de6d9dadfb4
Danone <mmmdanone04@gmail.com>
2025-04-18 12:46:16 +0300

    add colors and divide them into sets

--- app/src/main/java/com/example/quiz/ui/theme/Color.kt
+++ app/src/main/java/com/example/quiz/ui/theme/Color.kt
@@ -9,3 +9,39 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val Red = Color(0xfffb2c36)
+val RedLight = Color(0xffff6467)
+val RedDark = Color(0xffca3500)
+
+val Orange = Color(0xffff6900)
+val OrangeLight = Color(0xffff8904)
+val OrangeDark = Color(0xffbb4d00)
+
+val Yellow = Color(0xfff0b100)
+val YellowLight = Color(0xfffdc700)
+val YellowDark = Color(0xffd08700)
+
+val Lime = Color(0xff7ccf00)
+val LimeLight = Color(0xff9ae600)
+val LimeDark = Color(0xff5ea500)
+
+val Indigo = Color(0xff615fff)
+val IndigoLight = Color(0xff7c86ff)
+val IndigoDark = Color(0xff432dd7)
+
+val Purple = Color(0xffad46ff)
+val PurpleLight = Color(0xffc27aff)
+val PurpleDark = Color(0xff8200db)
+
+val Fuchsia = Color(0xffe12afb)
+val FuchsiaLight = Color(0xffed6aff)
+val FuchsiaDark = Color(0xffa800b7)
+
+val Pink = Color(0xfff6339a)
+val PinkLight = Color(0xfffb64b6)
+val PinkDark = Color(0xffc6005c)
+
+val Rose = Color(0xffff2056)
+val RoseLight = Color(0xffff637e)
+val RoseDark = Color(0xffc70036)
---
+++ app/src/main/java/com/example/quiz/ui/theme/ColorSets.kt
@@ -0,0 +1,83 @@
+package com.example.quiz.ui.theme
+
+import androidx.compose.runtime.Immutable
+import androidx.compose.ui.graphics.Color
+
+data object ColorSets {
+
+    val red = ColorSet(
+        main = Red,
+        light = RedLight,
+        dark = RedDark
+    )
+
+    val orange = ColorSet(
+        main = Orange,
+        light = OrangeLight,
+        dark = OrangeDark
+    )
+
+    val yellow = ColorSet(
+        main = Yellow,
+        light = YellowLight,
+        dark = YellowDark
+    )
+
+    val lime = ColorSet(
+        main = Lime,
+        light = LimeLight,
+        dark = LimeDark
+    )
+
+    val indigo = ColorSet(
+        main = Indigo,
+        light = IndigoLight,
+        dark = IndigoDark
+    )
+
+    val purple = ColorSet(
+        main = Purple,
+        light = PurpleLight,
+        dark = PurpleDark
+    )
+
+    val fuchsia = ColorSet(
+        main = Fuchsia,
+        light = FuchsiaLight,
+        dark = FuchsiaDark
+    )
+
+    val pink = ColorSet(
+        main = Pink,
+        light = PinkLight,
+        dark = PinkDark
+    )
+
+    val rose = ColorSet(
+        main = Rose,
+        light = RoseLight,
+        dark = RoseDark
+    )
+
+    val all = listOf(
+        red,
+        orange,
+        yellow,
+        lime,
+        indigo,
+        purple,
+        fuchsia,
+        pink,
+        rose
+    )
+
+    val random: ColorSet
+        get() = all.random()
+}
+
+@Immutable
+data class ColorSet(
+    val main: Color,
+    val light: Color,
+    val dark: Color
+)
