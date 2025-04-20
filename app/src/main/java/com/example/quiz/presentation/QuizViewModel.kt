e92b47095b417e359aa2321cc029f0b0d91f667c
Danone <mmmdanone04@gmail.com>
2025-04-20 17:28:18 +0300

    add quiz setup, start and processing functionality

--- app/src/main/java/com/example/quiz/presentation/QuizViewModel.kt
+++ app/src/main/java/com/example/quiz/presentation/QuizViewModel.kt
@@ -10,6 +10,7 @@ import com.example.quiz.domain.question.QuestionDifficulty
 import com.example.quiz.domain.question.QuestionType
 import com.example.quiz.domain.result.DataError
 import com.example.quiz.domain.result.Result
+import com.example.quiz.domain.session.QuizSession
 import com.example.quiz.presentation.message.MessageController
 import com.example.quiz.presentation.message.MessageEvent
 import kotlinx.coroutines.flow.MutableStateFlow
@@ -29,18 +30,115 @@ class QuizViewModel(
         initialValue = QuizState()
     )
 
+    private val pickedAnswers = mutableListOf<String>()
+
     init {
-        updateCategories()
+        loadCategories()
     }
 
     fun onEvent(event: QuizEvent) {
         when (event) {
-            QuizEvent.OnLoadCategoriesClick -> updateCategories()
+            QuizEvent.OnLoadCategoriesClick -> loadCategories()
 
             is QuizEvent.OnCategorySelected -> updatePickedCategory(event.category)
             is QuizEvent.OnQuestionAmountPick -> updatePickedQuestionAmount(event.amount)
             is QuizEvent.OnQuestionDifficultyPick -> updatePickedDifficulty(event.difficulty)
             is QuizEvent.OnQuestionTypePick -> updatePickedType(event.type)
+
+            QuizEvent.OnStartQuizClick -> startQuiz()
+            is QuizEvent.OnAnswerPicked -> processAnswer(event.answer)
+            QuizEvent.OnGoHomeClick -> goHome()
+        }
+    }
+
+    private fun goHome() {
+        _state.update {
+            it.copy(
+                currentRoute = QuizRoute.Home,
+                questions = emptyList()
+            )
+        }
+    }
+
+    private fun finishQuiz() {
+        val quizSession = QuizSession(
+            category = _state.value.pickedCategory,
+            questions = _state.value.questions,
+            pickedAnswers = pickedAnswers.toList()
+        )
+
+        pickedAnswers.clear()
+
+        _state.update {
+            it.copy(
+                currentRoute = QuizRoute.Results,
+                quizSession = quizSession
+            )
+        }
+    }
+
+    private fun processAnswer(answer: String) {
+        pickedAnswers += answer
+        if (pickedAnswers.size == _state.value.questions.size) {
+            finishQuiz()
+            return
+        }
+
+        _state.update {
+            it.copy(
+                currentQuestionNumber = it.currentQuestionNumber + 1
+            )
+        }
+    }
+
+    private fun startQuiz() {
+        viewModelScope.launch {
+            _state.update {
+                it.copy(
+                    isLoadingQuestions = true
+                )
+            }
+
+            val category = _state.value.pickedCategory
+            val amount = _state.value.pickedQuestionAmount
+            val difficulty = _state.value.pickedDifficulty
+            val type = _state.value.pickedType
+
+            val quiz = getQuestions(
+                amount = amount,
+                category = category,
+                difficulty = difficulty.takeIf { it != QuestionDifficulty.Any },
+                type = type.takeIf { it != QuestionType.Any }
+            )
+
+            /*val quiz = List(amount) {
+                Question(
+                    type = type,
+                    difficulty = difficulty,
+                    category = category.name,
+                    questionText = "Test question #$it",
+                    correctAnswer = "Correct answer",
+                    incorrectAnswers = List(3) {
+                        "Incorrect answer #$it"
+                    }
+                )
+            }*/
+
+            _state.update {
+                it.copy(
+                    isLoadingQuestions = false
+                )
+            }
+
+            quiz?.let { questions ->
+                _state.update {
+                    it.copy(
+                        currentRoute = QuizRoute.Quiz,
+                        questions = questions,
+                        currentQuestionNumber = 0
+                    )
+                }
+            }
         }
     }
 
@@ -76,18 +174,18 @@ class QuizViewModel(
         }
     }
 
-    private fun updateCategories() {
+    private fun loadCategories() {
         viewModelScope.launch {
             _state.update {
-                it.copy(isLoading = true)
+                it.copy(isLoadingCategories = true)
             }
 
-            //val categories = getCategories()
+            val categories = getCategories()
 
             _state.update {
                 it.copy(
-                    //categories = categories ?: it.categories,
-                    categories = List(30) { index ->
+                    categories = categories ?: it.categories,
+                    /*categories = List(30) { index ->
                         Category(
                             id = index, name = "Test #$index", group = when (index) {
                                 in 0..9 -> "Test category 1"
@@ -95,8 +193,8 @@ class QuizViewModel(
                                 else -> "Test category 3"
                             }
                         )
-                    },
-                    isLoading = false
+                    },*/
+                    isLoadingCategories = false
                 )
             }
         }
@@ -106,15 +204,7 @@ class QuizViewModel(
         return when (val result = quizProvider.getCategories()) {
             is Result.Success -> result.data
             is Result.Error -> {
-                val errorMessage = when (result.error) {
-                    DataError.Network.NoInternet -> R.string.no_internet_error
-                    DataError.Network.RequestTimeout -> R.string.request_timeout_error
-
-                    else -> R.string.unknown_error
-                }
-
-                MessageController.sendEvent(MessageEvent(errorMessage))
-
+                handleFetchingError(result.error)
                 null
             }
         }
@@ -129,21 +219,24 @@ class QuizViewModel(
         return when (val result = quizProvider.getQuiz(amount, category, difficulty, type)) {
             is Result.Success -> result.data
             is Result.Error -> {
-                val errorMessage = when (result.error) {
-                    DataError.Network.NoInternet -> R.string.no_internet_error
-                    DataError.Network.RequestTimeout -> R.string.request_timeout_error
-
-                    DataError.Network.InvalidDataFormat -> R.string.invalid_data_format_error
-                    DataError.Network.NoResults -> R.string.no_results_for_quiz_config_error
-                    DataError.Network.RateLimit -> R.string.rate_limit_error
-
-                    else -> R.string.unknown_error
-                }
-
-                MessageController.sendEvent(MessageEvent(errorMessage))
-
+                handleFetchingError(result.error)
                 null
             }
         }
+    }
+
+    private suspend fun handleFetchingError(error: DataError) {
+        val errorMessage = when (error) {
+            DataError.Network.NoInternet -> R.string.no_internet_error
+            DataError.Network.RequestTimeout -> R.string.request_timeout_error
+
+            DataError.Network.InvalidDataFormat -> R.string.invalid_data_format_error
+            DataError.Network.NoResults -> R.string.no_results_for_quiz_config_error
+            DataError.Network.RateLimit -> R.string.rate_limit_error
+
+            else -> R.string.unknown_error
+        }
+
+        MessageController.sendEvent(MessageEvent(errorMessage))
     }
 }
