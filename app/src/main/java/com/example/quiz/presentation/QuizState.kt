bf2fc7971e853bdd725f0d3e506e22924fab218b
Danone <mmmdanone04@gmail.com>
2025-04-20 17:26:19 +0300

    update quiz state with necessary fields

--- app/src/main/java/com/example/quiz/presentation/QuizState.kt
+++ app/src/main/java/com/example/quiz/presentation/QuizState.kt
@@ -1,14 +1,30 @@
 package com.example.quiz.presentation
 
 import com.example.quiz.domain.category.Category
+import com.example.quiz.domain.question.Question
 import com.example.quiz.domain.question.QuestionDifficulty
 import com.example.quiz.domain.question.QuestionType
+import com.example.quiz.domain.session.QuizSession
 
 data class QuizState(
-    val isLoading: Boolean = false,
+    val currentRoute: QuizRoute = QuizRoute.Home,
+
+    val isLoadingCategories: Boolean = false,
+    val isLoadingQuestions: Boolean = false,
+
     val categories: List<Category> = emptyList(),
+
     val pickedCategory: Category = Category(-1, "", ""),
     val pickedQuestionAmount: Int = 10,
-    val pickedDifficulty: QuestionDifficulty = QuestionDifficulty.Medium,
-    val pickedType: QuestionType = QuestionType.Any
+    val pickedDifficulty: QuestionDifficulty = QuestionDifficulty.Any,
+    val pickedType: QuestionType = QuestionType.Any,
+
+    val questions: List<Question> = emptyList(),
+    val currentQuestionNumber: Int = 0,
+
+    val quizSession: QuizSession = QuizSession(
+        category = pickedCategory,
+        questions = emptyList(),
+        pickedAnswers = emptyList()
+    )
 )
